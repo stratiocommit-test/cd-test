cf89fd2cfee03108dbd775897325f2da0dac9e77
acelorio-stratio <acelorio@stratio.com>
2019-08-29 14:12:41 +0200

    Attempt parallel stages

--- src/test/java/com/stratio/qa/cdtest/CdIT.java
+++ src/test/java/com/stratio/qa/cdtest/CdIT.java
@@ -6,16 +6,17 @@ import org.testng.annotations.Test;
 public class CdIT {
 
     @Test(groups={"integration"})
-    public void exampleIntegrationTest(){
+    public void exampleIntegrationTest() throws Exception{
         String[] args = new String[2];
         CdTest.main(args);
         try {
-            System.out.println("Waiting for 10 seconds");
-            Thread.sleep(100000);
+            System.out.println("Waiting for 1 seconds");
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         Assert.assertEquals("YES", "YES");
+        throw new Exception("On purpose exception");
     }
 
     @Test
